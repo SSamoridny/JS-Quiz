1c5504875418d4f78f3bce32e5ab1dd4ae1a6fdc
Shaun Samoridny <ssamoridny@gmail.com>
2021-05-11 17:03:35 -0600

    added score and the framework for future questions

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const correctAnswers = ['B', 'B', 'B', 'B']
-
 const form = document.querySelector('.quiz-form')
+const result = document.querySelector('.result')
 
 form.addEventListener('submit', (e) => {
     e.preventDefault()
@@ -15,6 +15,19 @@ form.addEventListener('submit', (e) => {
         }
     })
 
-    console.log(score)
+    // show results
+    scrollTo(0,0)
+    
+    result.classList.remove('d-none')
+
+    let output = 0;
+    const timer = setInterval(() => {
+        result.querySelector('span').textContent = `${output}%`;
+        if(output === score){
+            clearInterval(timer);
+        } else {
+            output++;
+        }
+    }, 10)
 
 })
